0d50ee757c50491cd21c403f4fe9fa4d98b6dae5
J.P. Hamilton <instantjp@gmail.com>
2025-07-17 09:31:41 -0500

    remove dead code

--- Zork1/Zork1GUE.cs
+++ Zork1/Zork1GUE.cs
@@ -35,56 +35,5 @@ public class Zork1GUE : Story
         MovePlayer.To<WestOfHouse>();
 
         SetLast.Object<Mailbox>();
-
-        ////TESTING
-        //var lamp = Objects.Get<BrassLantern>();
-        //lamp.On = true;
-        //lamp.Light = true;
-        //Player.Add(lamp);
-        //Player.Add(Objects.Get<AirPump>());
-        //Flags.DamOpen = true;
-        //Flags.LowTide = true;
-        //MovePlayer.To<DeepCanyon>();
-
-        //// --------------------  END GAME TEST
-        //var window = Objects.Get<KitchenWindow>();
-        //window.Open = true;
-        //var trophy_case = Objects.Get<TrophyCase>();
-        //trophy_case.Open = true;
-
-        //var treasures = Objects.All.Where(x => x.TakeValue > 0 && x is not Room).ToList();
-
-        //var rooms = Objects.All.Where(x => x.TakeValue > 0 && x is Room).ToList();
-
-        //foreach (var room in rooms)
-        //{
-        //    Score.ScoreObject(room);
-        //}
-
-        //var score = State.Score;
-
-        //var inCase = treasures.Take(18).ToList();
-        //foreach (var t in inCase)
-        //{
-        //    Score.ScoreObject(t);
-        //    Score.AddTrophy(t);
-        //    t.Move(trophy_case);
-        //    t.Concealed = false;
-        //    t.TakeValue = 0;
-        //    t.Visited = true;
-        //}
-
-        //var inInv = treasures.Skip(18).ToList();
-        //foreach (var t in inInv)
-        //{
-        //    Score.ScoreObject(t);
-        //    t.Move(Player.Instance);
-        //    t.Concealed = false;
-        //    t.TakeValue = 0;
-        //    t.Visited = true;
-        //}
-
-        //MovePlayer.To<LivingRoom>();
-
     }
 }
